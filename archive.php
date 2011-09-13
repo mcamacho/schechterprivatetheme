b2be221d2b7b6c072cdb424ce9ed34111282519d
Mauricio Camacho <mcamacho@bbmg.com>
2011-09-13 17:23:05 -0400

    clean files

--- archive.php
+++ archive.php
@@ -45,10 +45,10 @@
     <?php else : ?>
         <article id="post-0" class="post no-results not-found">
             <header class="entry-header">
-                <h1 class="entry-title"><?php _e( 'Nothing Found', 'schechtertheme' ); ?></h1>
+                <h1 class="entry-title"><?php _e( 'Nothing Found', 'schechterthemeprivate' ); ?></h1>
             </header><!-- .entry-header -->
             <div class="entry-content">
-                <p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'schechtertheme' ); ?></p>
+                <p><?php _e( 'Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'schechterthemeprivate' ); ?></p>
                 <?php get_search_form(); ?>
             </div><!-- .entry-content -->
         </article><!-- #post-0 -->
--- header.php
+++ header.php
@@ -40,7 +40,7 @@
 
 	// Add a page number if necessary:
 	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'schechtertheme' ), max( $paged, $page ) );
+		echo ' | ' . sprintf( __( 'Page %s', 'schechterthemeprivate' ), max( $paged, $page ) );
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
@@ -55,17 +55,8 @@
 <![endif]-->
 <?php wp_enqueue_script("jquery"); ?>
 <?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
 	if ( is_singular() && get_option( 'thread_comments' ) )
 		wp_enqueue_script( 'comment-reply' );
-
-	/* Always have wp_head() just before the closing </head>
-	 * tag of your theme, or you will break many plugins, which
-	 * generally use this hook to add elements to <head> such
-	 * as styles, scripts, and meta tags.
-	 */
 	wp_head();
 ?>
 </head>
